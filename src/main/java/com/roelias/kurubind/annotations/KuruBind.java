a06247aea0d9e225c22eb5cf0eaa917fe0d80568
elialm7 <metall65eli@gmail.com>
2026-01-02 00:30:18 -0300

    feat: Annotation to detect kurubindEntities

---
+++ src/main/java/com/roelias/kurubind/annotations/KuruBind.java
@@ -0,0 +1,15 @@
+package com.roelias.kurubind.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * Marks a class as a kurubind entity
+ * Classes with this annotation will be handled byu kurubindRowMapper
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+public @interface KuruBind {
+
+}
