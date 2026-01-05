8f8e48037f81b6c46b6920076656927ad6b70676
elialm7 <metall65eli@gmail.com>
2026-01-05 16:58:26 -0300

    cleanup: removing old code

--- src/main/java/com/roelias/legacy/base/JdbiProvider.java
+++
@@ -1,7 +0,0 @@
-package com.roelias.legacy.base;
-
-import org.jdbi.v3.core.Jdbi;
-
-public interface JdbiProvider {
-    Jdbi getJdbi();
-}
--- src/main/java/com/roelias/legacy/base/KurubindModule.java
+++
@@ -1,6 +0,0 @@
-package com.roelias.legacy.base;
-
-@FunctionalInterface
-public interface KurubindModule {
-    void configure(RegistryCollector registries);
-}
